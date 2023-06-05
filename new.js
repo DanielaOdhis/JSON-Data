c9fc384b589882be6de9e82b9a12a942688b54eb
Daniela Odhiambo <98682114+DanielaOdhis@users.noreply.github.com>
2023-06-05 11:05:28 +0300

    Update new.js

--- new.js
+++ new.js
@@ -1,8 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
+  response.setHeader('Content-Type', 'text/html');
+
+  fs.readFile('index.html', 'utf8', (error, data) => {
+    if (error) {
+      response.statusCode = 500;
+      response.end('Internal Server Error');
+    } else {
+      response.end(data);
+    }
+  });
 });
 
 const port = 5000;
