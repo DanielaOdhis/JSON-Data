e5e05ce9cbb6ca0fef99b973806e671b9841b955
Daniela Odhiambo <98682114+DanielaOdhis@users.noreply.github.com>
2023-06-05 11:53:28 +0300

    Update new.js

--- new.js
+++ new.js
@@ -1,18 +1,50 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/html');
+  const { url } = request;
 
-  fs.readFile('index.html', 'utf8', (error, data) => {
-    if (error) {
-      response.statusCode = 500;
-      response.end('Internal Server Error');
-    } else {
-      response.end(data);
-    }
-  });
+  if (url === '/') {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+
+    fs.readFile('index.html', 'utf8', (error, data) => {
+      if (error) {
+        response.statusCode = 500;
+        response.end('Internal Server Error');
+      } else {
+        response.end(data);
+      }
+    });
+  } else if (url === '/style.css') {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/css');
+
+    fs.readFile('style.css', 'utf8', (error, data) => {
+      if (error) {
+        response.statusCode = 500;
+        response.end('Internal Server Error');
+      } else {
+        response.end(data);
+      }
+    });
+  } else if (url === '/script.js') {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/javascript');
+
+    fs.readFile('script.js', 'utf8', (error, data) => {
+      if (error) {
+        response.statusCode = 500;
+        response.end('Internal Server Error');
+      } else {
+        response.end(data);
+      }
+    });
+  } else {
+    response.statusCode = 404;
+    response.end('Not Found');
+  }
 });
 
 const port = 5000;
